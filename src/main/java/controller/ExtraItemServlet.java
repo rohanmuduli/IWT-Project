614cdad83cc71ad37e08ff712dfaf1f8d1220c40
rohanmuduli <rohanmuduli18@gmail.com>
2026-04-27 19:30:07 +0530

    Remove all files except index.html and register.html in proper src/main/webapp directory

--- src/main/java/controller/ExtraItemServlet.java
+++
@@ -1,8 +0,0 @@
-package controller;
-
-import jakarta.servlet.http.HttpServlet;
-
-// Dummy class to satisfy Eclipse's publish mechanism without causing a WebServlet conflict
-public class ExtraItemServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
-}
